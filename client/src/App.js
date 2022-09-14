4c3674615f3938d273b31c3a1fa08a508c714426
LastCarol <lastcarol0315@gmail.com>
2022-09-14 20:31:33 +0900

    #1 FrontEnd initial setting

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>hello world</h2>
+      <h2>Hello world</h2>
     </div>
   );
 }
--- client/src/lib/store.js
+++
@@ -1,2 +0,0 @@
-import create from "zustand";
-const useStore = create((set) => ({}))
