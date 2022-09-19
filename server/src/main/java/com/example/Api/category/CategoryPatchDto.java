bb4fdc29c8368c191587b3922b126096b7b91994
fasdf1 <whddls1747@gmail.com>
2022-09-19 15:12:44 +0900

    #7[Feat/categoryController] ] controller /삭제/조회 기능 수정

---
+++ server/src/main/java/com/example/Api/category/CategoryPatchDto.java
@@ -0,0 +1,9 @@
+package com.example.Api.category;
+
+
+import lombok.Data;
+
+@Data
+public class CategoryPostDto {
+    private String categoryName;
+}
--- server/src/main/java/com/example/Api/category/CategoryPostDto.java
+++ server/src/main/java/com/example/Api/category/CategoryPostDto.java
@@ -1,9 +1,2 @@
-package com.example.Api.category;
-
-
-import lombok.Data;
-
-@Data
-public class CategoryPostDto {
-    private String categoryName;
+package com.example.Api.category;public class CategoryPostDto {
 }
