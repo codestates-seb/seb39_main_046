2656c472371dc753fad19dc4af69a489cc859677
LastCarol <lastcarol0315@gmail.com>
2022-09-21 16:52:02 +0900

    #28 [Feat/carashel] 캐러셀 스타일링 및 상품 정렬

--- client/src/components/bestProduct/RecomendProduct.js
+++ client/src/components/bestProduct/RecomendProduct.js
@@ -6,6 +6,40 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const RecomendProduct = () => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
     <div>
       <RecoProduct>
@@ -15,7 +49,79 @@ const RecomendProduct = () => {
       </RecoProduct>
       {/* 캐러셀 적용 하는 곳  */}
       <RecoProductsbar>
-        <Slider>
+        <Slider {...settings}>
+        <ProductSection>
+          <RecoProducts />
+          <ProductsEx>
+            <ProductCategory>category</ProductCategory>
+            <ProductName>Product name</ProductName>
+            <div></div>
+            <ProductPrice>5,300원</ProductPrice>
+          </ProductsEx>
+        </ProductSection>
+        <ProductSection>
+          <RecoProducts />
+          <ProductsEx>
+            <ProductCategory>category</ProductCategory>
+            <ProductName>Product name</ProductName>
+            <div></div>
+            <ProductPrice>5,300원</ProductPrice>
+          </ProductsEx>
+        </ProductSection>
+        <ProductSection>
+          <RecoProducts />
+          <ProductsEx>
+            <ProductCategory>category</ProductCategory>
+            <ProductName>Product name</ProductName>
+            <div></div>
+            <ProductPrice>5,300원</ProductPrice>
+          </ProductsEx>
+        </ProductSection>
+        <ProductSection>
+          <RecoProducts />
+          <ProductsEx>
+            <ProductCategory>category</ProductCategory>
+            <ProductName>Product name</ProductName>
+            <div></div>
+            <ProductPrice>5,300원</ProductPrice>
+          </ProductsEx>
+        </ProductSection>
+        <ProductSection>
+          <RecoProducts />
+          <ProductsEx>
+            <ProductCategory>category</ProductCategory>
+            <ProductName>Product name</ProductName>
+            <div></div>
+            <ProductPrice>5,300원</ProductPrice>
+          </ProductsEx>
+        </ProductSection>
+        <ProductSection>
+          <RecoProducts />
+          <ProductsEx>
+            <ProductCategory>category</ProductCategory>
+            <ProductName>Product name</ProductName>
+            <div></div>
+            <ProductPrice>5,300원</ProductPrice>
+          </ProductsEx>
+        </ProductSection>
+        <ProductSection>
+          <RecoProducts />
+          <ProductsEx>
+            <ProductCategory>category</ProductCategory>
+            <ProductName>Product name</ProductName>
+            <div></div>
+            <ProductPrice>5,300원</ProductPrice>
+          </ProductsEx>
+        </ProductSection>
+        <ProductSection>
+          <RecoProducts />
+          <ProductsEx>
+            <ProductCategory>category</ProductCategory>
+            <ProductName>Product name</ProductName>
+            <div></div>
+            <ProductPrice>5,300원</ProductPrice>
+          </ProductsEx>
+        </ProductSection>
         <ProductSection>
           <RecoProducts />
           <ProductsEx>
