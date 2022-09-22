d2bd91e6fbe648a2da76f6a106774e8fd1c8abcf
LastCarol <lastcarol0315@gmail.com>
2022-09-23 06:36:00 +0900

    #38 [Feat/Mypage] 개인정보 찜꽁바구니 편BTI 바 목업

---
+++ client/src/components/myPage/PersonalInfo.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import styled from 'styled-components';
+import Userimg from '../../assets/images/userinfo/Userimg.jpg';
+import Button from '../common/button/Button';
+import TextInput from '../common/input/TextInput';
+
+const PersonalInfo = () => {
+
+    const userName = "리코";
+    const welcommsg = " 님, 안녕하세요 :)";
+    const email = "reco@recostore.com"; 
+    const userImg = Userimg;
+
+    return (
+        <TopDiv>
+            <UserInfo>
+                <Titlediv>
+                    <UserName>{userName}</UserName>
+                    <Welcome>{welcommsg}</Welcome>
+                </Titlediv>
+                <UserPassing>
+                    <UserExer>
+                        <img src={userImg} alt="프로필 사진" />
+                        <Button>수정</Button>
+                        <span>ID:{email}</span>
+                    </UserExer>
+                    <UserForm>
+                        <p>닉네임</p>                     
+                        <TextInput/>
+                        <Button>완료</Button>
+                        <p>패스워드</p>
+                        <TextInput/>
+                        <Button >수정</Button>
+                        <p>패스워드 확인</p>
+                        <TextInput/>                        
+                    </UserForm>
+                </UserPassing>                                
+            </UserInfo>
+        </TopDiv>
+    );
+};
+
+export default PersonalInfo;
+
+const TopDiv = styled.div `
+    padding-top : ${({ theme }) => theme.paddings.xxxl};
+`
+const UserInfo = styled.div `
+    margin-top:30px;
+    text-align:center;
+    width:100%;
+    height: 600px;
+`
+const Titlediv = styled.div`
+    text-align:center;
+`
+const UserName = styled.span`
+    color: ${({ theme }) => theme.colors.Blue_040};
+    font-size: ${({ theme }) => theme.fontSizes.titleSize};
+    font-weight: 700;
+    line-height:160%;
+`
+
+const UserPassing = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+`
+
+const UserExer = styled.div`
+    width:193px;
+    height:193px;
+    img{
+        width:150px;
+        height:150px;
+    }
+    Button{
+        margin-top:-10px;
+    }
+    span{
+        padding-top:${({ theme }) => theme.paddings.base};
+        font-size: ${({ theme }) => theme.fontSizes.base};
+        color: ${({ theme }) => theme.colors.Blue_040};
+        font-weight:700;
+    }
+`
+
+const Welcome = styled.span`
+    color: ${({ theme }) => theme.colors.Gray_050};
+    font-size: ${({ theme }) => theme.fontSizes.titleSize};
+    font-weight:700;
+    line-height:160%;
+`
+
+const UserForm = styled.form`
+    text-align:left;
+    width:390px;
+    height:264px;
+    margin-top: ${({ theme }) => theme.paddings.xxxl};
+    p{
+        padding-left:${({ theme }) => theme.paddings.xl};
+        position:relative;
+        font-size: ${({ theme }) => theme.fontSizes.xs};
+        font-weight:500;
+    }
+    input{
+        margin-bottom: ${({ theme }) => theme.paddings.xxxl};
+    }
+`
+
+
+
+
+
+
---
+++ client/src/components/myPage/ProductBasket.js
@@ -0,0 +1,251 @@
+import React from 'react';
+import styled from 'styled-components';
+import "../bestProduct/slick-theme.css";
+import "../bestProduct/slick.css";
+import Slider from "react-slick";
+import titleimg from '../../assets/images/userinfo/title.png';
+
+
+const ProductBasket = () => {
+
+    const userName = "리코";
+    const more = "더보기 >"
+    const settings = {
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        speed: 2000,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      };
+
+
+    return (
+        <Topdiv>
+            <BasketTitle>
+                <section>
+                    <img src={titleimg} alt="이틀 이미지"/>
+                    <Username>{userName}<span>님의 찜꽁바구니</span></Username>
+                    <p>{more}</p>                 
+                </section>
+            </BasketTitle>
+            <CarashelContent>
+            <Slider {...settings}>                
+                <ProductSection>
+                    <RecoProducts />                                        
+                    <ProductsEx>                    
+                        <ProductCategory>category</ProductCategory>
+                        <ProductName>Product name</ProductName>                        
+                        <div></div>
+                        <ProductPrice>5,300원</ProductPrice>
+                    </ProductsEx>
+                </ProductSection>
+                <ProductSection>                    
+                    <RecoProducts />
+                    <ProductsEx>
+                        <ProductCategory>category</ProductCategory>
+                        <ProductName>Product name</ProductName>
+                        <div></div>
+                        <ProductPrice>5,300원</ProductPrice>
+                    </ProductsEx>
+                </ProductSection>
+                <ProductSection>
+                <RecoProducts />
+            <ProductsEx>
+              <ProductCategory>category</ProductCategory>
+              <ProductName>Product name</ProductName>
+              <div></div>
+              <ProductPrice>5,300원</ProductPrice>
+            </ProductsEx>
+          </ProductSection>
+          <ProductSection>
+            <RecoProducts />
+            <ProductsEx>
+              <ProductCategory>category</ProductCategory>
+              <ProductName>Product name</ProductName>
+              <div></div>
+              <ProductPrice>5,300원</ProductPrice>
+            </ProductsEx>
+          </ProductSection>
+          <ProductSection>
+            <RecoProducts />
+            <ProductsEx>
+              <ProductCategory>category</ProductCategory>
+              <ProductName>Product name</ProductName>
+              <div></div>
+              <ProductPrice>5,300원</ProductPrice>
+            </ProductsEx>
+          </ProductSection>
+          <ProductSection>
+            <RecoProducts />
+            <ProductsEx>
+              <ProductCategory>category</ProductCategory>
+              <ProductName>Product name</ProductName>
+              <div></div>
+              <ProductPrice>5,300원</ProductPrice>
+            </ProductsEx>
+          </ProductSection>
+          <ProductSection>
+            <RecoProducts />
+            <ProductsEx>
+              <ProductCategory>category</ProductCategory>
+              <ProductName>Product name</ProductName>
+              <div></div>
+              <ProductPrice>5,300원</ProductPrice>
+            </ProductsEx>
+          </ProductSection>
+          <ProductSection>
+            <RecoProducts />
+            <ProductsEx>
+              <ProductCategory>category</ProductCategory>
+              <ProductName>Product name</ProductName>
+              <div></div>
+              <ProductPrice>5,300원</ProductPrice>
+            </ProductsEx>
+          </ProductSection>
+          <ProductSection>
+            <RecoProducts />
+            <ProductsEx>
+              <ProductCategory>category</ProductCategory>
+              <ProductName>Product name</ProductName>
+              <div></div>
+              <ProductPrice>5,300원</ProductPrice>
+            </ProductsEx>
+          </ProductSection>
+          <ProductSection>
+            <RecoProducts />
+            <ProductsEx>
+              <ProductCategory>category</ProductCategory>
+              <ProductName>Product name</ProductName>
+              <div></div>
+              <ProductPrice>5,300원</ProductPrice>
+            </ProductsEx>
+          </ProductSection>
+        </Slider>
+            </CarashelContent>
+        </Topdiv>
+    );
+};
+
+
+const Topdiv = styled.div`
+    margin-left:300px;
+    margin-right:300px;
+`
+const BasketTitle = styled.section`
+    width:100%;
+    section{
+        display:flex;
+        margin: 0 auto;
+        justify-content: space-around;
+    }
+    img{
+        /* margin-top:${({ theme }) => theme.margins.xl}; */
+        width: 119px;
+        height:140px;
+    }
+    p{
+        font-size:${({ theme }) => theme.fontSizes.small};
+        color:${({ theme }) => theme.colors.Orange_040};
+        padding-top:${({ theme }) => theme.paddings.xl};
+        cursor: pointer;
+    }
+
+`
+const Username = styled.span`
+    font-size: ${({ theme }) => theme.fontSizes.titleSize};
+    font-weight: 700;
+    text-align:center;
+    color:${({ theme }) => theme.colors.Blue_040};
+    span{
+        font-size: ${({ theme }) => theme.fontSizes.titleSize};
+        font-weight: 700;
+        color:${({ theme }) => theme.colors.Gray_090};           
+    }
+`
+const CarashelContent = styled.section`
+  margin: 0 auto;
+  width: 1280px;
+  margin-bottom: 100px;
+  margin-right: 250px;
+`
+
+
+const ProductSection = styled.section`
+  width: 240px;
+  height: 355px;
+  border-radius: 20px;
+  margin-right: 14px;
+`;
+
+const ProductsEx = styled.div`
+  padding: 10px 15px;
+  border-radius: 0 0 20px 20px;
+  div {
+    width: 100%;
+    height: 1px;
+    background-color: ${({ theme }) => theme.colors.Gray_010};
+    margin: 5px 0;
+  }
+`;
+
+const RecoProducts = styled.div`
+  width: 100%;
+  height: 240px;
+  background: url("/character/상품준비 안됫음 .png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 70%;
+  border-radius: 20px 20px 0 0;
+  background-color: #fff9f3;
+`;
+
+
+const ProductCategory = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes.xs};
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.Orange_040};
+`;
+const ProductName = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes.base};
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.Gray_090};
+`;
+const ProductPrice = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes.base};
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.Gray_030};
+`;
+
+
+
+
+export default ProductBasket;
