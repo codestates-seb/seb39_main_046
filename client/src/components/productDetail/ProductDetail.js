a8f5a7f6e24caaa52a023c030e747ee10c6be078
LastCarol <lastcarol0315@gmail.com>
2022-09-25 01:50:11 +0900

    #38 #52 [Feat/Style] 마이페이지 상품상세페이지 목업

---
+++ client/src/components/productDetail/ProductDetail.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+import HeartButton from '../common/button/HeartButton';
+
+import Noimg from '../../assets/images/userinfo/Noimg.png';
+import link from '../../assets/images/common/link.png';
+
+
+const ProductDetail = () => {
+  return (
+    <MainContent>
+      <ProductImage>
+        <div className='hearttitle'>
+          <span><HeartButton/></span>
+        </div>
+        <div className='sharetitle'>
+          <span><img src={link}  width="30px" height="30px" alt="링크이미지"/></span>
+        </div>              
+        <img src={Noimg} alt="이미지 없어요"/>
+      </ProductImage>
+      
+    </MainContent>
+  );
+};
+
+export default ProductDetail;
+
+const MainContent = styled.div`
+  width: 590px;
+  height: 590px;
+`
+const ProductImage = styled.section`
+  width: 100%;
+  display:flex;
+  flex-direction: column;
+  justify-content:center;
+  align-items:center;
+  .hearttitle{
+    width:100%;
+    text-align:right;
+  }
+  .sharetitle{
+    width:100%;
+    text-align:right;
+  }
+`
+const 
