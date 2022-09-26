6935abc17f5704a289c03363c614d4cbc17525c7
LastCarol <lastcarol0315@gmail.com>
2022-09-26 22:34:34 +0900

    #59[Feat/function] 로그인 회원가입 구현2

--- client/src/lib/api/useLogin.jsx
+++ client/src/lib/api/useLogin.jsx
@@ -2,7 +2,9 @@ import {useMutation } from 'react-query';
 import axios from 'axios';
 
 const loginperson = (log) => {
-  return axios.post('/login', log )
+  return axios.post('/member/login', log).then(function (res){
+    console.log(res.data);
+  }).then((data) => console.log(data));
 }
 
 
--- client/src/lib/api/useSignup.jsx
+++ client/src/lib/api/useSignup.jsx
@@ -1,8 +1,10 @@
-import {useQuery, useMutation } from 'react-query';
+import {useMutation } from 'react-query';
 import axios from 'axios';
 
 const addPerson = (person) => {
-  return axios.post('/member/signup', person )
+  return axios.post('/member/signup', person).then(function(res){
+    console.log(res.data);
+  })
 }
 
 export const useSignup = () => {
