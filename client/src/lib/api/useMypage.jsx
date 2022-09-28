47b649eaf65b63807cc81a5a5d7a28ea4ea43367
choiseoeun-GoUp <79019104+choiseoeun-GoUp@users.noreply.github.com>
2022-09-28 10:56:55 +0900

    #61 [Fix/MyPage] axiosInstance로 common axios파일 변경 및 api 파일 확장자 변경

--- client/src/lib/api/useMypage.jsx
+++ client/src/lib/api/useMypage.jsx
@@ -1,44 +1,43 @@
 import React from "react";
 import { useQuery, useQueryClient } from "react-query";
-import axios from "axios";
 import useStore from "../store";
+import axiosInstance from "../../utils/axiosInastance";
+import Loading from "../../components/common/loading/Loading";
 
 const getInfomation = async (logInfo) => {
-  const { data } = await axios.get("/member/myPage", {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: logInfo,
-    },
-  });
-  return data;
+    const { data } = await axiosInstance.get("/member/myPage", {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: logInfo,
+        },
+    });
+    return data;
 };
 
 export function useMypage() {
-  // const queryClient = useQueryClient();
+    // const queryClient = useQueryClient();
 
-  const { logInfo } = useStore();
-  const { status, data, isError, isLoading, isFetching } = useQuery(
-    ["infos", logInfo],
-    () => getInfomation(logInfo),
-    {
-      keepPreviousData: true,
-      staleTime: 2000,
-      onSuccess: (data) => {
-        console.log(data);
-      },
-      onError: (e) => {
-        console.log(e.message);
-      },
+    const { logInfo } = useStore();
+    const { status, data, error, isFetching } = useQuery(["infos", logInfo], () => getInfomation(logInfo), {
+        keepPreviousData: true,
+        staleTime: 2000,
+        onSuccess: (data) => {
+            console.log(data);
+        },
+        onError: (e) => {
+            console.log(e.message);
+        },
+    });
+
+    if (status === "loading") {
+        return <Loading />;
     }
-  );
 
-  if (status === "loading") return <h3>로딩중</h3>;
-
-  if (status === "error") {
-    return <h3>에러발생</h3>;
-  }
-  if (isFetching) {
-    return <h3>로딩중</h3>;
-  }
-  return data;
+    if (status === "error") {
+        return <span>Error: {error.message}</span>;
+    }
+    if (isFetching) {
+        return <Loading />;
+    }
+    return data;
 }
