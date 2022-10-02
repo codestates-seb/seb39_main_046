f31fba6eed9eb1a8e85d0bf598df167fd32cb2f1
fasdf1 <whddls1747@gmail.com>
2022-10-02 17:12:38 +0900

    #71 [fix:Member] - valid Condition

--- server/src/main/java/com/example/Api/member/MemberPatchDtoN.java
+++ server/src/main/java/com/example/Api/member/MemberPatchDtoN.java
@@ -11,6 +11,6 @@ public class MemberPatchDtoN {
 
 
     @NotBlank(message = "nickName은 공백일 수 없습니다.")
-    @Size(min = 1, max = 10, message = "nickName은 1 ~ 10자로 설정해주세요.")
+    @Size(min = 2, max = 10, message = "nickName은 2 ~ 10자로 설정해주세요.")
     private String nickName;
 }
--- server/src/main/java/com/example/Api/member/MemberPatchDtoP.java
+++ server/src/main/java/com/example/Api/member/MemberPatchDtoP.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.Size;
 public class MemberPatchDtoP {
 
     @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
-    @Size(min = 1, max = 50, message = "상품명은 1 ~ 50자 이여야 합니다.")
-    @Pattern(regexp = "^.*(?=^.{8,15}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",message = "비밀번호는 숫자, 영문자, 특수문자가 포함된 8~15자리로 설정해주세요.")
+    @Size(min = 8, max = 16, message = "비밀번호는 8 ~ 16자로 설정해주세요.")
+    @Pattern(regexp = "^.*(?=^.{8,16}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",message = "비밀번호는 숫자, 영문자, 특수문자가 포함된 8~16자리로 설정해주세요.")
     private String password;
 }
