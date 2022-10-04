8a1c8fcdfe0266d3b4a1a9b69277d59ec9e0ce13
choiseoeun-GoUp <79019104+choiseoeun-GoUp@users.noreply.github.com>
2022-10-04 18:01:57 +0900

    로그인 요청 url 변경

--- client/src/lib/api/useLogin.jsx
+++ client/src/lib/api/useLogin.jsx
@@ -2,7 +2,7 @@ import { useMutation } from "react-query";
 import axiosInstance from "../../utils/axiosInastance";
 
 const loginperson = (log) => {
-    return axiosInstance.post(`/login`, log).then((res) => (console.log(res.data)));
+    return axiosInstance.post(`/api/login`, log).then((res) => console.log(res.data));
 };
 
 export const useLogin = (onSuccess, onError) => {
