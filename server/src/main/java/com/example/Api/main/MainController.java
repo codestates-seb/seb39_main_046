12b1e81ba2c7318885dd2838958ae9a22d0108d3
gu-geonhoe <rjsghl92@gmail.com>
2022-10-04 16:07:26 +0900

    cors 변경2

--- server/src/main/java/com/example/Api/main/MainController.java
+++ server/src/main/java/com/example/Api/main/MainController.java
@@ -20,6 +20,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,7 @@ import java.util.*;
 @RestController
 @RequestMapping("/main")
 @Validated
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 public class MainController {
 
     private final ProductService productService;
