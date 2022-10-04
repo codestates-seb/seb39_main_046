af837869b5afd9a79c0b3742b0358725860fbcf0
LastCarol <lastcarol0315@gmail.com>
2022-10-04 15:52:04 +0900

    로그인 api 롤백

--- client/src/lib/api/useLogin.jsx
+++ client/src/lib/api/useLogin.jsx
@@ -2,7 +2,7 @@ import { useMutation } from "react-query";
 import axiosInstance from "../../utils/axiosInastance";
 
 const loginperson = (log) => {
-    return axiosInstance.post(`/api/login`, log)
+    return axiosInstance.post(`/login`, log)
 };
 
 export const useLogin = (onSuccess, onError) => {
