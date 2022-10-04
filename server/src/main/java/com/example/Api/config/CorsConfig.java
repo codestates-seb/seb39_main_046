cfa21430e0af708367f7ac60e429c36125d46161
gu-geonhoe <rjsghl92@gmail.com>
2022-10-04 22:35:39 +0900

    test10

--- server/src/main/java/com/example/Api/config/CorsConfig.java
+++ server/src/main/java/com/example/Api/config/CorsConfig.java
@@ -17,10 +17,12 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true); //서버가 응답할 때 json을 자바스크립트에서 처리할 수 있도록 설정합니다 -> 무슨 의미?
+        config.addAllowedOriginPattern("*"); //모든 ip 응답허용
+        config.addAllowedHeader("*"); //모든 Header 응답허용
+        config.addAllowedMethod("*"); //모든 Http Request Method 응답허용
         config.addExposedHeader("Authorization");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
