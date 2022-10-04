164dd106bb51450c9667ee10279f4cabba1d10d4
gu-geonhoe <rjsghl92@gmail.com>
2022-10-04 16:27:34 +0900

    cors 변경6

--- server/src/main/java/com/example/Api/filter/JwtAuthenticationFilter.java
+++ server/src/main/java/com/example/Api/filter/JwtAuthenticationFilter.java
@@ -63,11 +63,11 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         long a = principalDetails.getMember().getMemberId();
         String userRole = principalDetails.getMember().getRoles();
         map.put(JwtProperties.HEADER_PREFIX,JwtProperties.TOKEN_PREFIX + jwtToken);
-        map.put("role",userRole);
-        map.put("msg" , "success");
+//        map.put("role",userRole);
+//        map.put("msg" , "success");
         Gson gson = new Gson();
         String jsonString = gson.toJson(map);
-        response.getWriter().println(jsonString);
+        response.getWriter().println(JwtProperties.TOKEN_PREFIX + jwtToken);
 
     }
 }
