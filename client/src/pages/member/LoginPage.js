ba33c02c88105514bc576d3ac8ed014453c6e6e1
LastCarol <lastcarol0315@gmail.com>
2022-10-04 16:21:40 +0900

    로그인 재수정

--- client/src/pages/member/LoginPage.js
+++ client/src/pages/member/LoginPage.js
@@ -22,8 +22,7 @@ const Login = () => {
     const onSuccess = (res) => {
         console.log(userName);
         alert(`${userName}환영합니다.`);
-        console.log(res.data);
-        // sessionStorage.setItem("token", res.Authorization);
+        sessionStorage.setItem("token", res.data);
         navigate("/");
         window.location.reload();
     };
