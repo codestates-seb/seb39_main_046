7769602627505e71171324b2047a916ff1727205
LastCarol <lastcarol0315@gmail.com>
2022-10-04 18:17:42 +0900

    회원가입 롤백

--- client/src/lib/api/useSignup.jsx
+++ client/src/lib/api/useSignup.jsx
@@ -2,7 +2,7 @@ import { useMutation } from "react-query";
 import axiosInstance from "../../utils/axiosInastance";
 
 const addPerson = (person) => {
-    return axiosInstance.get("/member/signup", person);
+    return axiosInstance.post("/member/signup", person);
 };
 
 export const useSignup = (onSuccess, onError) => {
