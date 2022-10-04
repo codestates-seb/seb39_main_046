5e575d55bcc001f406b1dd16578207c0852c1525
gu-geonhoe <rjsghl92@gmail.com>
2022-10-04 18:41:00 +0900

    토큰 변경 13

--- server/src/main/java/com/example/Api/config/CorsConfig.java
+++ server/src/main/java/com/example/Api/config/CorsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -17,11 +18,11 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("https://www.recostore.shop/"));
+        config.setAllowedOrigins(Arrays.asList("*"));
         config.addAllowedOrigin("*");
         config.addExposedHeader("Authorization");
         config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        config.setAllowedMethods(Arrays.asList("GET","POST", "PATCH", "DELETE"));
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
--- server/src/main/java/com/example/Api/config/SecurityConfig.java
+++ server/src/main/java/com/example/Api/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -33,6 +34,7 @@ public class SecurityConfig {
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
+
                 .apply(new CustomDsl())
                 .and()
                 .authorizeRequests()
