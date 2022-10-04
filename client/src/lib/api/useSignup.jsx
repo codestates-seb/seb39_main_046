5cc4dc9ad8174c5c97107baf343addae8b69080d
LastCarol <lastcarol0315@gmail.com>
2022-10-04 18:11:09 +0900

    로그추가

--- client/src/lib/api/useSignup.jsx
+++ client/src/lib/api/useSignup.jsx
@@ -2,7 +2,7 @@ import { useMutation } from "react-query";
 import axiosInstance from "../../utils/axiosInastance";
 
 const addPerson = (person) => {
-    return axiosInstance.get("/member/signup");
+    return axiosInstance.get("/member/signup", person);
 };
 
 export const useSignup = (onSuccess, onError) => {
