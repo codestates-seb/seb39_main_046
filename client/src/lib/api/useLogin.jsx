177f9b6cf3c220ed91ecf142d8ccdf06181b191d
LastCarol <lastcarol0315@gmail.com>
2022-10-04 17:25:52 +0900

    헤드추가

--- client/src/lib/api/useLogin.jsx
+++ client/src/lib/api/useLogin.jsx
@@ -2,7 +2,7 @@ import { useMutation } from "react-query";
 import axiosInstance from "../../utils/axiosInastance";
 
 const loginperson = (log) => {
-    return axiosInstance.post(`/login`, log).then((res) => (console.log(res.data)));
+    return axiosInstance.get(`/login`, log).then((res) => (console.log(res.data)));
 };
 
 export const useLogin = (onSuccess, onError) => {
