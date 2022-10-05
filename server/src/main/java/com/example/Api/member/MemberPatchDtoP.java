33da73b0a787f9c5a2c10244b9eccf986f9c31b0
fasdf1 <whddls1747@gmail.com>
2022-10-05 14:25:02 +0900

    member- password : 특수문자 * 추가

--- server/src/main/java/com/example/Api/member/MemberPatchDtoP.java
+++ server/src/main/java/com/example/Api/member/MemberPatchDtoP.java
@@ -12,6 +12,6 @@ public class MemberPatchDtoP {
 
     @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
     @Size(min = 8, max = 16, message = "비밀번호는 8 ~ 16자로 설정해주세요.")
-    @Pattern(regexp = "^.*(?=^.{8,16}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",message = "비밀번호는 숫자, 영문자, 특수문자가 포함된 8~16자리로 설정해주세요.")
+    @Pattern(regexp = "^.*(?=^.{8,16}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*+=]).*$",message = "비밀번호는 숫자, 영문자, 특수문자가 포함된 8~16자리로 설정해주세요.")
     private String password;
 }
