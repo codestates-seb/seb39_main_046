8a9d2d26723402c67064122ca30390c669591d22
fasdf1 <whddls1747@gmail.com>
2022-10-05 11:08:50 +0900

    productPatchDto : NotBlank -> NotNull

--- server/src/main/java/com/example/Api/product/ProductPatchDto.java
+++ server/src/main/java/com/example/Api/product/ProductPatchDto.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Data
 public class ProductPatchDto {
 
-    @NotBlank(message = "상품명은 공백일 수 없습니다.")
+    @NotNull(message = "상품명은 공백일 수 없습니다.")
     @Size(min = 1, max = 50, message = "상품명은 1 ~ 50자 이여야 합니다.")
     private String productName;
 
@@ -22,11 +22,11 @@ public class ProductPatchDto {
     @DecimalMin(value = "1", message = "가격은 0보다 커야 합니다.")
     private BigDecimal price;
 
-    @NotBlank(message = "카테고리명은 공백일 수 없습니다.")
+    @NotNull(message = "카테고리명은 공백일 수 없습니다.")
     @Size(min = 1, max = 50, message = "카테고리명은 1 ~ 50자 이여야 합니다.")
     private String categoryName;
 
-    @NotBlank(message = "회사명은 공백일 수 없습니다.")
+    @NotNull(message = "회사명은 공백일 수 없습니다.")
     @Pattern(regexp = "(CU|GS25|7-ELVEN)",message = "회사명에는 CU, GS25,7-ELEVEN만 입력 가능합니다")
     @Size(min = 1, max = 10, message = "회사명은 1 ~ 10자 이여야 합니다.")
     private  String company;
