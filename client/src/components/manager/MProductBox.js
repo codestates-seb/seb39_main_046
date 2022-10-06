9aa105ae989979c37fa97f0110a1298910d173fd
LastCarol <lastcarol0315@gmail.com>
2022-10-06 13:32:18 +0900

    버셀오류테스트

--- client/src/components/manager/MProductBox.js
+++ client/src/components/manager/MProductBox.js
@@ -14,7 +14,7 @@ const MProductBox = ({ data, setIsOpen }) => {
 
     const { mutate: ProductDelete } = DeleteProduct();
     const deleteFunc = () => {
-        if (window.confirm("정말 삭제하시겠습니까?")) {
+        if (window.confirm("정말 삭제하시겠습니까??")) {
             ProductDelete(data.productId);
             navigate(`/admin`);
         } else {
