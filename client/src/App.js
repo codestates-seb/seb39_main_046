836d52acb43347e03e93aa3d3a058af449ebc3c5
LastCarol <lastcarol0315@gmail.com>
2022-10-17 00:46:21 +0900

    [FIX] netlify 배포시도

--- client/src/App.js
+++ client/src/App.js
@@ -26,12 +26,12 @@ function App() {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Main />}></Route>
-                    {/* <Route path="/findstore" element={<FindStore />}></Route> */}
-                    {/* <Route path="/foodtest" element={<FootTest />}>
+                    <Route path="/findstore" element={<FindStore />}></Route>
+                    <Route path="/foodtest" element={<FootTest />}>
                         <Route index element={<FoodTestHome />}></Route>
                         <Route path="question" element={<FoodTestQuestion />}></Route>
                         <Route path="result" element={<FoodTestResult />}></Route>
-                    </Route> */}
+                    </Route>
                     <Route path="/login" element={<Login />}></Route>
                     <Route path="/signup" element={<SignUp />}></Route>
                     <Route path="/signup/admin" element={<SignUpAdmin />}></Route>
